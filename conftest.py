869156e2412d313f7e4f4cb298813ace873653b4
Nikitin Vitaliy <nikitin20@mail.ru>
2024-04-01 23:21:54 +0300

    reload file

---
+++ conftest.py
@@ -0,0 +1,49 @@
+import pytest
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FFOptions
+from selenium.webdriver.edge.options import Options as EdgeOptions
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", default="chrome")
+    parser.addoption("--headless", action="store_true")
+    parser.addoption("--url", default="http://192.168.1.5:8081/")
+
+
+@pytest.fixture()
+def browser(request):
+    browser_name = request.config.getoption("--browser")
+    headless = request.config.getoption("--headless")
+    base_url = request.config.getoption("--url")
+
+    driver = None
+
+    if browser_name == "chrome":
+        options = ChromeOptions()
+        if headless:
+            options.add_argument("--headless=new")
+        service = ChromeService()
+        driver = webdriver.Chrome(service=service, options=options)
+
+    elif browser_name == "ff":
+        options = FFOptions()
+        if headless:
+            options.add_argument("-headless")
+        driver = webdriver.Firefox(options=options)
+
+    elif browser_name == "edge":
+        options = EdgeOptions()
+        if headless:
+            options.add_argument("--headless=new")
+        driver = webdriver.Edge(options=options)
+
+    driver.maximize_window()
+
+    driver.base_url = base_url
+
+    yield driver
+
+    driver.close()
